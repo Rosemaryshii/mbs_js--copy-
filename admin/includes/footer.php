9ce56c9df607a293b5bb90c06ddee600fea428dc
edwin nyongesa <arjannky@gmail.com>
2023-05-23 06:17:02 +0300

    glasses are now updatable

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -14,6 +14,7 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/chart.js/chart.umd.js"></script>
